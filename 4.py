3688b1a6ab7521abd8a41277a26b2c44a8c3e784
Pedro Diogo <p.diogo@eyeo.com>
2023-12-04 12:05:40 +0100

    second star for day 4

--- 4.py
+++ 4.py
@@ -1,6 +1,9 @@
 with open('inputs/4.txt', 'r') as file:
     total = 0
+    instances = [1 for _ in range(0, 205)]
+    pos = -1
     for line in file:
+        pos+=1
         #ignore the first part "Card ###:"
         line = line.strip().split(':')[1]
         winningNumbers, myNumbers = line.split('|') 
@@ -15,6 +18,12 @@ with open('inputs/4.txt', 'r') as file:
                 corrects += 1
         if corrects > 0:
             total += (2**(corrects-1))
-    print(total)
+            for i in range(pos+1, pos+1+corrects):
+                instances[i] += instances[pos]
+        print(instances)
+    print("total points: ", total)
+
+    print("total cards: ", sum(instances))
+
 
 
