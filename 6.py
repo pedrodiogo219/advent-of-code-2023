6f987f67e3529bd520533d24564510539655cbf0
Pedro Diogo <p.diogo@eyeo.com>
2023-12-06 13:12:12 +0100

    day 6

---
+++ 6.py
@@ -0,0 +1,16 @@
+import math
+with open('inputs/6.txt', 'r') as file:
+
+    time = int("".join(file.readline().strip().split()[1:]))
+    # times = [int(x) for x in file.readline().strip().split()[1:]]
+    distance = int("".join(file.readline().strip().split()[1:]))
+    
+    print(time, distance)
+
+    delta = math.sqrt((time*time) - (4)*(-1)*(-distance))
+    
+    A = math.ceil((-time + delta) / -2)
+    B = math.floor((-time - delta) / -2)
+
+    print(A, B)
+    print(B - A + 1)    
