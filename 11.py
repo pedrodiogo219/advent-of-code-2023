4e7ce90c83cc137a9d21ce5638db4f8ac8a1bb21
Pedro Diogo <p.diogo@eyeo.com>
2023-12-12 14:02:12 +0100

    day 11 part 2

--- 11.py
+++ 11.py
@@ -11,34 +11,43 @@ colCont = [0 for _ in range(len(plane[0]))]
 for i in range(len(plane)):
     for j in range(len(plane[i])):
         if plane[i][j] == '#':
+            galaxies.append((i, j))
             rowCont[i] += 1
             colCont[j] += 1
 
-for i in range(len(colCont)-1, -1, -1):
-    if colCont[i] == 0:
-        for row in range(len(plane)):
-            plane[row].insert(i, '.')
 
-for i in range(len(rowCont)-1, -1, -1):
+accumRow = [0 for _ in range(len(plane))]
+accumCol = [0 for _ in range(len(plane[0]))]
+
+expansionFactor = 1000000
+
+for i in range(1, len(plane)):
+    accumRow[i] = accumRow[i-1]
     if rowCont[i] == 0:
-        plane.insert(i, ['.' for _ in range(len(plane[0]))])
+        accumRow[i] += expansionFactor - 1
 
-# for i in range(len(plane)):
-#     print("".join(plane[i]))
+for i in range(1, len(plane[0])):
+    accumCol[i] = accumCol[i-1]
+    if colCont[i] == 0:
+        accumCol[i] += expansionFactor-1
 
-for i in range(len(plane)):
-    for j in range(len(plane[i])):
-        if plane[i][j] == '#':
-            galaxies.append((i, j))
-
-# print(galaxies)
+# for galaxy in galaxies:
+#     x, y = galaxy
+#     print((x,y))
+#     print(accumRow[x], accumCol[y])
 
 total = 0
 for i in range(len(galaxies)):
     x, y = galaxies[i]
     for j in range(i+1, len(galaxies)):
         nx, ny = galaxies[j]
-        dist = abs(x-nx) + abs(y-ny)
+
+        newX = x + accumRow[x]
+        newNx = nx + accumRow[nx]
+        newY = y + accumCol[y]
+        newNy = ny + accumCol[ny]
+
+        dist = abs(newX-newNx) + abs(newY-newNy)
         total += dist
         # print("dist from ", i, " to ", j, " is ", dist)
 
